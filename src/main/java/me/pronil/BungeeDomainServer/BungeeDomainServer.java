691d1d0f029748541429c13c7e0752483000ce9e
BuildTools <unconfigured@null.spigotmc.org>
2022-09-29 23:09:13 +0530

    Improvements

--- src/main/java/me/pronil/BungeeDomainServer/BungeeDomainServer.java
+++ src/main/java/me/pronil/BungeeDomainServer/BungeeDomainServer.java
@@ -1,7 +1,15 @@
 package me.pronil.BungeeDomainServer;
 
+<<<<<<< HEAD
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
+=======
+import net.md_5.bungee.api.Callback;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.ServerPing;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+>>>>>>> b337919 (Improvements)
 import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -10,11 +18,19 @@ import net.md_5.bungee.api.plugin.PluginManager;
 import net.md_5.bungee.event.EventHandler;
 
 import java.util.Arrays;
+<<<<<<< HEAD
+=======
+import java.util.HashMap;
+>>>>>>> b337919 (Improvements)
 
 
 public class BungeeDomainServer extends Plugin implements Listener {
 
+<<<<<<< HEAD
 private String[] server;
+=======
+    public static HashMap<String, String[]> serverMap = new HashMap<>();
+>>>>>>> b337919 (Improvements)
 
     @Override
     public void onEnable() {
@@ -23,6 +39,7 @@ private String[] server;
     }
 
 
+<<<<<<< HEAD
 
     @EventHandler
     public void onPreLogin(PreLoginEvent event) {
@@ -41,4 +58,36 @@ private String[] server;
 
 
 
+=======
+    @EventHandler
+    public void onPreLogin(PreLoginEvent event) {
+        String domain = event.getConnection().getVirtualHost().getHostName().toLowerCase();
+        serverMap.put(event.getConnection().getName(), domain.split("\\.", 2));
+
+    }
+
+    @EventHandler
+    public void onServerConnect(final ServerConnectEvent event) {
+        if (serverMap.containsKey(event.getPlayer().getName())) {
+            final ServerInfo target = ProxyServer.getInstance().getServerInfo(serverMap.get(event.getPlayer().getName())[0]);
+            if (getProxy().getServers().containsKey(serverMap.get(event.getPlayer().getName())[0])) {
+                event.setTarget(target);
+                getProxy().getServers().get(serverMap.get(event.getPlayer().getName())[0]).ping(new Callback<ServerPing>() {
+                    @Override
+                    public void done(ServerPing result, Throwable error) {
+                        if (error != null) {
+                            ProxyServer.getInstance().getConsole().sendMessage("Couldn't connect " + event.getPlayer().getName() + " to " + serverMap.get(event.getPlayer().getName())[0] + " as the server is offline");
+                        } else {
+                            ProxyServer.getInstance().getConsole().sendMessage("Connected " + event.getPlayer().getName() + " to " + serverMap.get(event.getPlayer().getName())[0]);
+                            serverMap.remove(event.getPlayer().getName());
+                            event.setCancelled(true);
+                        }
+                    }
+                });
+            } else {
+                ProxyServer.getInstance().getConsole().sendMessage("Could not connect " + event.getPlayer().getName() + " to " + serverMap.get(event.getPlayer().getName())[0] + " as the server does not exist in BungeeCord Config.yml");
+           }
+        }
+    }
+>>>>>>> b337919 (Improvements)
 }
